1532050880bd0ddfffae82c2867e19c18cc6488e
d.stolbinskiy <denver7074@yandex.ru>
2025-09-21 14:50:36 +0300

    homework-2

--- src/app/components/to-do-list/to-do-list.ts
+++ src/app/components/to-do-list/to-do-list.ts
@@ -1,10 +1,17 @@
 import {Component, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {ToDoListItemComponent} from '../to-do-list-item-component/to-do-list-item-component';
+import {Task} from '../../interfaces/task.intarface';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-to-do-list',
   imports: [
-    FormsModule
+    FormsModule,
+    ToDoListItemComponent,
+    MatFormFieldModule,
+    MatInput
   ],
   templateUrl: './to-do-list.html',
   standalone: true,
@@ -13,7 +20,7 @@ import {FormsModule} from '@angular/forms';
 export class ToDoList {
   newTaskName = '';
 
-  tasks = signal([
+  tasks = signal<Task[]>([
     {id: 1, name: 'Изучить Angular'},
     {id: 2, name: 'Освоить Bootstrap'},
     {id: 3, name: 'Создать приложение'}
@@ -29,9 +36,17 @@ export class ToDoList {
     if (name.trim()) {
       this.tasks.update(current => [
         ...current,
-        { id: Date.now(), name: name.trim() }
+        { id: this.getMaxId(), name: name.trim() }
       ]);
       this.newTaskName = '';
     }
+  }
+
+  getMaxId(): number {
+    const currentTasks = this.tasks();
+    if (currentTasks.length === 0)
+      return 1;
+
+    return Math.max(...currentTasks.map(task => task.id)) + 1;
   }
 }
